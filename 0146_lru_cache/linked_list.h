3c307af5c3730eed3aa70363a07935f8e1c9ecfd
Tianxing Zhang <anakinzhang96@gmail.com>
2021-08-27 23:14:11 +0800

    enrich linked_list.h

--- 0146_lru_cache/linked_list.h
+++ 0146_lru_cache/linked_list.h
@@ -1,3 +1,23 @@
+/**
+ * container_of - cast a member of a structure out to the containing structure
+ * @ptr:	the pointer to the member.
+ * @type:	the type of the container struct this is embedded in.
+ * @member:	the name of the member within the struct.
+ */
+#define container_of(ptr, type, member) \
+	((type *)((char *)(ptr) - (size_t)&(((type *)0)->member)))
+
+/**
+ * list_entry - get the struct for this entry
+ * @ptr:	the &struct list_head pointer.
+ * @type:	the type of the struct this is embedded in.
+ * @member:	the name of the list_head within the struct.
+ *
+ * Note, that list is expected to be not empty.
+ */
+#define list_entry(ptr, type, member) \
+	container_of(ptr, type, member)
+
 struct list_head {
 	struct list_head *next, *prev;
 }
@@ -7,11 +27,21 @@ static inline void INIT_LIST_HEAD(struct list_head *list)
 	list->next = list->prev = list;
 }
 
+/**
+ * list_empty - tests whether a list is empty
+ * @head:	the list to test.
+ */
 static inline void list_empty(const struct list_head *head)
 {
 	return (head->next == head);
 }
 
+/**
+ * Insert a new entry between two known consecutive entries.
+ *
+ * This is only for internal list manipulation where we know
+ * the prev/next entries already!
+ */
 static inline void __list_add(struct list_head *new, struct list_head *prev, struct list_head *next)
 {
 	next->prev = new;
@@ -20,36 +50,80 @@ static inline void __list_add(struct list_head *new, struct list_head *prev, str
 	prev->next = new;
 }
 
-static inline void list_add(struct list_head *_new, struct list_head *head)
+/**
+ * list_add - add a new entry
+ * @new:	new entry to be added
+ * @head:	list head to add it after
+ *
+ * Insert a new entry after the specified head.
+ * This is good for implementing stacks.
+ */
+static inline void list_add(struct list_head *new, struct list_head *head)
 {
-	__list_add(_new, head, head->next);
+	__list_add(new, head, head->next);
 }
 
-static inline void list_add_tail(struct list_head *_new, struct list_head *head)
+/**
+ * list_add_tail - add a new entry
+ * @new:	new entry to be added
+ * @head:	list head to add it before
+ *
+ * Insert a new entry before the specified head.
+ * This is useful for implementing queues.
+ */
+static inline void list_add_tail(struct list_head *new, struct list_head *head)
 {
-	__list_add(_new, head->prev, head);
+	__list_add(new, head->prev, head);
 }
 
-static inline void __list_del(struct list_head *entry)
+/**
+ * Delete a list entry by making the prev/next entries
+ * point to each other.
+ *
+ * This is only for internal list manipulation where we know
+ * the prev/next entries already!
+ */
+static inline void __list_del(struct list_head *prev, struct list_head *next)
 {
-	entry->next->prev = entry->prev;
-	entry->prev->next = entry->next;
+	next->prev = prev;
+	prev->next = next;
 }
 
+static inline void __list_del_entry(struct list_head *entry)
+{
+	__list_del(entry->prev, entry->next);
+}
+
+/**
+ * list_del - deletes entry from list.
+ * @entry:	the element to delete from the list.
+ * Note: list_empty() on entry does not return true after this, the entry is
+ * in an undefined state.
+ */
 static inline void list_del(struct list_head *entry)
 {
-	__list_del(entry);
+	__list_del_entry(entry);
 	entry->next = entry->prev = NULL;
 }
 
+/**
+ * list_move - delete from one list and add as another's head.
+ * @list:	the entry to move
+ * @head:	the head that will precede our entry
+ */
 static inline void list_move(struct list_head *list, struct list_head *head)
 {
-	__list_del(list);
+	__list_del_entry(list);
 	list_add(list, head);
 }
 
-static inline void list_move_tail(struct list_head *entry, struct list_head *head)
+/**
+ * list_move_tail - delete from one list and add as another's tail.
+ * @list:	the entry to move
+ * @head:	the head that will follow our entry
+ */
+static inline void list_move_tail(struct list_head *list, struct list_head *head)
 {
-	__list_del(entry);
-	list_add_tail(entry, head);
+	__list_del_entry(list);
+	list_add_tail(list, head);
 }
